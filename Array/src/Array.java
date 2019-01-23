1604c12b26f656d2ae76ca482f0b755d21805754
fengchunsgit <1055688803@qq.com>
2019-01-23 22:30:59 +0800

    'array_add'

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -26,4 +26,27 @@ public class Array {
         return size==0;
     }
 
+    public void addLast(int e){
+        add(size,e);
+    }
+
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    public void add(int index,int e){
+        if(size==data.length){
+            throw new IllegalArgumentException("add faild.");
+        }
+        if(index<0 || index>size){
+            throw new IllegalArgumentException("add faild.index>=0 and index<=size");
+        }
+        for (int i=size-1;i>=index;i--){
+            data[i+1]=data[i];
+            data[index]=e;
+            size++;
+        }
+
+    }
+
 }
